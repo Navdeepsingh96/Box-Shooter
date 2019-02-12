aa9e849f0ee844cf7fa7735d01ba2fc037530ef3
Navdeep Singh <navdeep.singh96@hotmail.com>
2019-02-11 20:13:57 -0500

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/BasicRotate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicRotate : MonoBehaviour {
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // spins the gameObject around Y axis
+        if (doSpin)
+        {
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+
+        // make cosine wave motion for gameObject in Y axis
+        if(doMotion)
+        {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+	}
+}
